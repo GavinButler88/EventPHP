aa2783fa373309e9d2dd90f7153eb972c296679c
Gavin Butler <gavin-j-b@hotmail.com>
2015-03-11 13:39:45 +0000

    fixed errors i made with the classes. Location and event

--- Location.php
+++ Location.php
@@ -1,8 +1,54 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//
+class Location {
+    
+    private $locationid;
+    private $nameOfLocation;
+    private $address;
+    private $maxCapacity;
+    private $locationManagerName;
+    private $locationManagerAddress;
+    private $locationManagerNumber;
+
+    public function __construct($lid, $nol, $a, $mc, $lmn, $lma, $lmno) {
+        $this->locationid = $lid;
+        $this->nameOfLocation = $nol;
+        $this->address = $a;
+        $this->maxCapacity = $mc;
+        $this->locationManagerName = $lmn;
+        $this->locationManagerAddress = $lma;
+        $this->locationManagerNumber = $lmno;
+    }
+    
+    public function getLocationid() {
+        return $this->locationid;
+    }
+    
+    public function getNameOfLocation() {
+        return $this->nameOfLocation;
+    }
+
+    public function getAddress() {
+        return $this->address;
+    }
+
+    public function getMaxCapacity() {
+        return $this->maxCapacity;
+    }
+
+    public function getLocationManagerName() {
+        return $this->lmn;
+    }
+
+    public function getLocationManagerAddress() {
+        return $this->lma;
+    }
+
+    public function getLocationManagerNumber() {
+        return $this->lmno;
+    }
+
+}
+
 
